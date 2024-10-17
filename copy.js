74182a3088544dd29e616307c217f2f42c8caab3
Trần Kim Quang <135234833+kimquangst5@users.noreply.github.com>
2024-10-17 19:13:31 +0700

    Thêm file vercel vào thư mục dist

--- copy.js
+++ copy.js
@@ -6,3 +6,7 @@ fs.copy('views', 'dist/views')
 fs.copy('public', 'dist/public')
 	.then(() => console.log('success Public!'))
 	.catch(err => console.error(err))
+
+	fs.copy('vercel.json', 'dist/vercel.json')
+	.then(() => console.log('success vercel.json!'))
+	.catch(err => console.error(err))
